82c1037e0b3d1aef1b74a0099e6b40bbbc32de73
Julia Evans <julevans93@users.noreply.github.com>
2019-12-16 23:29:47 -0500

    working on models...account ab class complete

--- src/main/java/com/zoost/Zoost/Models/Account_Type.java
+++ src/main/java/com/zoost/Zoost/Models/Account_Type.java
@@ -1,4 +1,32 @@
 package com.zoost.Zoost.Models;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Account_Type")
 public class Account_Type {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "account_status_id")
+    private int account_status_id;
+
+    @Column(name = "account_type")
+    private String type;
+
+    public int getAccount_status_id() {
+        return account_status_id;
+    }
+
+    public void setAccount_status_id(int account_status_id) {
+        this.account_status_id = account_status_id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
--- src/main/java/com/zoost/Zoost/Models/User.java
+++ src/main/java/com/zoost/Zoost/Models/User.java
@@ -1,4 +1,89 @@
 package com.zoost.Zoost.Models;
 
-public class User {
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "User")
+public abstract class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "account_type")
+    private String account_type;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "account_type"))
+    private Set<Role> roles;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAccount_type() {
+        return account_type;
+    }
+
+    public void setAccount_type(String account_type) {
+        this.account_type = account_type;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
