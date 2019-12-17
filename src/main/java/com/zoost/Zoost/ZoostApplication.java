ce14864f7aaa0de0839b84061db351b6cff8cc8b
Julia Evans <56592566+julevans93@users.noreply.github.com>
2019-12-16 21:33:03 -0500

    Add files via upload

---
+++ src/main/java/com/zoost/Zoost/ZoostApplication.java
@@ -0,0 +1,13 @@
+package com.zoost.Zoost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zoost/Zoost/ZoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoost.Zoost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
